7d76f8838e2493cb538cfa2adfd033d493af2f71
Dan Pletzke <dpletzke@gmail.com>
2020-09-24 23:20:19 +0000

    fetch IP and get coords working

---
+++ index.js
@@ -0,0 +1,34 @@
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+
+/* fetchCoordsByIP test */
+// const fetchCoordsCB = (err, data) => {
+//   if (err) {
+//       console.error(err);
+//     }
+//   if (!err) {
+//     const { latitude, longitude } = data;
+//     console.log(latitude, longitude);
+//   }
+// }
+
+// fetchCoordsByIP('98.226.100.163', fetchCoordsCB);
+
+
+
+
+
+
+/* fetchMyIP Test */
+// const ipFetchCB = (err, ipAddress) => {
+//   if (err) {
+//     console.error(err);
+//   }
+//   if (!err) {
+//     console.log(ipAddress.ip);
+//   }
+// }
+
+
+// fetchMyIP(ipFetchCB);
+
